f67d22db0d667e0f133508c1d104142db547cc49
hiushi <blazincalf@gmail.com>
2015-08-26 12:06:45 -0400

    add multiple flash cards

--- public/app.js
+++ public/app.js
@@ -1,14 +1,14 @@
 var angularApp = angular.module('FlashCards', []);
 
 angularApp.controller('FlashCardController', function ($scope) {
-  $scope.flashCard = {
-    question: 'What is Angular?',
-    answers: [
-      { text: 'A front-end framework for great power!', correct: true },
-      { text: 'Something lame, who cares, whatever.', correct: false },
-      { text: 'Some kind of fish, right?', correct: false }
-    ]
-  };
+  // $scope.flashCard = {
+  //   question: 'What is Angular?',
+  //   answers: [
+  //     { text: 'A front-end framework for great power!', correct: true },
+  //     { text: 'Something lame, who cares, whatever.', correct: false },
+  //     { text: 'Some kind of fish, right?', correct: false }
+  //   ]
+  // };
 
   $scope.answerQuestion = function (answer) {
     if ($scope.answered) return;
